64d01f7b8ea6dd55f4146a12ff0cb4f5e05bbdc4
Sergeim <smiroshnikov@securitydam.com>
2018-08-02 19:38:58 +0300

    Initial commit

---
+++ Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Grades
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please Work Motherfucker ! ");
+            var var1 = 10;
+
+
+            while (var1 != 0)
+            {
+                if (var1 != 0)
+                {
+                    Console.WriteLine("Substracting.... " + var1.ToString());
+                    var1 = var1 - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Yeah it is !");
+                }
+            }
+
+            Console.WriteLine("Finito ! ....  " + var1.ToString());
+        }
+    }
+}
